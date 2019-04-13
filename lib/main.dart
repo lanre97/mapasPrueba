c392cb4b8816f5e38c0c87c3ad01ed2209609c95
lanre97 <flavioontaneda@gmail.com>
2019-04-13 14:42:40 -0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,27 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main(){
+
+  final DocumentReference=Firestore.instance.collection("posiciones").document("prueba");
+  Position currentPosition=new Position(151.0,15989.0);
+
+
+}
+
+class Position {
+  double latitude;
+  double longitude;
+  Position(this.longitude,this.latitude);
+}
+
+void add(Position position, DocumentReference documentReference){
+  Map<String, String>data=<String, String>{
+    "latitude":position.latitude.toString(),
+    "longitude":position.longitude.toString()
+  };
+  documentReference.setData(data).whenComplete((){
+    print("print added");
+  }).catchError((e)=>print(e));
+}
 
